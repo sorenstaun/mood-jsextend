0b0aed792244e08beb6c856fe13bf8afe071e42b
sorenstaun <113976403+sorenstaun@users.noreply.github.com>
2024-05-28 11:13:51 +0200

    Update SSJavaScriptLibrary.js

--- js/SSJavaScriptLibrary.js
+++ js/SSJavaScriptLibrary.js
@@ -450,7 +450,8 @@ function ssObserveNotificationBar(searchFor, functionref) {
  */
 function ssSetTexteditorValue(name, val) {
   $(".mood-node-name-" + name + " input")[0].value = val;
-  $(".mood-node-name-" + name + " input")[1].value = val;
+  if ($(".mood-node-name-" + name + " input").length >1)
+     $(".mood-node-name-" + name + " input")[1].value = val;
 }
 
 /**
